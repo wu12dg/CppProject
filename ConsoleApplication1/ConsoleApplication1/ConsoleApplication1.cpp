1e31a12e0337ae690ee7921440221a3ee182dc20
jyetby <2802923195@qq.com>
2026-04-28 15:34:01 +0800

    fff

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -17,13 +17,11 @@ public:
 class Student {
 protected:
 	char sex;
-	string name1;
 	float score;
 
 public:
-	Student(char s,string n1,float sc) {
+	Student(char s,float sc) {
 		sex = s;
-		name1 = n1;
 		score = sc;
 	}
 
@@ -32,7 +30,7 @@ class Graduate :public Teacher, public Student {
 protected:
 	int wage;
 public:
-	Graduate(string n, int a, string t, char s, string n1, float sc,int w):Teacher(n,a,t),Student(s,n1,sc),wage(w){}
+	Graduate(string n, int a, string t, char s, float sc,int w):Teacher(n,a,t),Student(s,sc),wage(w){}
 	void show() {
 		cout << "姓名：" << name << "\n 年龄：" << age << "\n 职称：" << title << "\n 性别：" << sex << "\n 分数:" << score << "\n 津贴" << wage << endl;
 	}
@@ -40,8 +38,8 @@ public:
 
 int main()
 {
-	Graduate gra1("张三", 21, "讲师", "M",  92.5f, 1000);
-
+	Graduate gra1( "张三", 21, "讲师", 'M', 92.5f, 1000);
+	gra1.show();
 
 	return 0;
 }
