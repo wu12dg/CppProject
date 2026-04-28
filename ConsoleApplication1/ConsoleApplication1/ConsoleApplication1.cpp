57197791b08e75b6bd578ee3c2a640a095da037d
Administrator <Administrator@JF509-05>
2026-04-28 14:55:43 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+#include<string>
+class Teacher {
+protected:
+	string name;
+	int age;
+	string title;
+public:
+	Teacher(string n,int a,string t) {
+		name = n;
+		age = a;
+		title = t;
+	}
+
+};
+class Student {
+protected:
+	char sex;
+	string name1;
+	int score;
+
+public:
+	Student(char s,string n1,int sc) {
+		sex = s;
+		name1 = n1;
+		score = sc;
+	}
+
+};
+class Draduate :public Teacher, public Student {
+protected:
+public:
+};
+
+int main()
+{
+
+
+
+	return 0;
+}
