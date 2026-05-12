afe912c349be5d28a8d9974d36cbe5e4f018a0e2
Administrator <Administrator@JF509-05>
2026-05-12 16:15:15 +0800

    05.12

--- ConsoleApplication0512t/ConsoleApplication0512t/ConsoleApplication0512t.cpp
+++ ConsoleApplication0512t/ConsoleApplication0512t/ConsoleApplication0512t.cpp
@@ -1,20 +1,49 @@
-﻿// ConsoleApplication0512t.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
 #include <iostream>
+using namespace std;
+#include<string>
+class Family {
+private:
+	string flowers;
+public:
+	Family(string name="鲜花") {
+		flowers = name;
+	}
+	string getname() {
+		return flowers;
+	}
+	virtual void like() {
+		cout << "家人喜欢不同的花" << endl;
+	}
+};
+class Father :public Family {
+public:
+	Father(string name="仙人掌"):Family(name){}
+	void like() {
+		cout << "爸爸喜欢" << getname() << endl;
+	}
+};
+class Mother :public Family {
 
+};
+class Daught :public Family {
+
+};
+//全局函数likeFlower
+void likeFlower(Family *pr) {
+	pr->like();
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	Family fa, * p;
+	Father f; 
+	p = &fa;
+	p->like();
+	p = &f;
+	p->like();
+
+	cout << "使用普通函数调用虚函数：" << endl;
+	likeFlower(&fa);
+	likeFlower(&f);
+	return 0;
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
