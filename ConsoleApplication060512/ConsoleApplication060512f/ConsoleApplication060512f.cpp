89a8dd9e2f0258271951a7533fc2e00a7e5a969d
Administrator <Administrator@JF509-05>
2026-05-12 16:01:01 +0800

    05.12做了些新科了

---
+++ ConsoleApplication060512/ConsoleApplication060512f/ConsoleApplication060512f.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+class Animal {
+public:
+	virtual void makeSound() const{
+		cout << "动物会发出声音" << endl;
+	}
+};
+class Dog :public Animal {
+	void makeSound() {
+		cout << "汪汪" << endl;
+	}
+};
+class Cat :public Animal {
+public:
+	void makeSound() {
+		cout << "喵喵" << endl;
+	}
+};
+//统一接口,对象引用
+void LetItSpeak(const Animal& animal) {
+	animal.makeSound();
+}
+
+int main()
+{
+	Animal animal;
+	Cat cat;
+	Dog dog;
+
+	Animal* p = &animal;
+	p->makeSound();
+	p = &cat;
+	p->makeSound();
+	p = &dog;
+	p->makeSound();
+
+	cout << "------------------" << endl;
+
+	return 0;
+}
---
+++ ConsoleApplication060512/ConsoleApplication06512/ConsoleApplication06512.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+// 声明基类Student
+class Student {
+public:
+    Student(int, string, float); // 声明构造函数
+    void display();              // 声明输出函数
+protected:                     // 受保护成员,派生类可以访问
+    int num;
+    string name;
+    float score;
+};
+
+// Student类成员函数的实现
+Student::Student(int n, string nam, float s) // 定义构造函数
+{
+    num = n;
+    name = nam;
+    score = s;
+}
+
+void Student::display() // 定义输出函数
+{
+    cout << "num:" << num << "\n name:" << name << "\n score:" << score << endl;
+}
+// 声明公用派生类Graduate
+class Graduate : public Student {
+public:
+    Graduate(int, string, float, float); // 声明构造函数
+    void display();                      // 与基类的输出函数同名
+private:
+    float wage;
+};
+// Graduate类成员函数的实现
+Graduate::Graduate(int n, string nam, float s, float w) : Student(n, nam, s), wage(w) {}
+void Graduate::display() // 定义输出函数
+{
+    cout << "num:" << num << "\n name:" << name << "\n score:" << score
+        << "\n wage=" << wage << endl;
+}
+// 主函数
+int main() {
+    Student stud1(1001, "Li", 87.5);          // 定义Student类对象stud1
+    Graduate grad1(2001, "Wang", 98.5, 1200); // 定义Graduate类对象grad1
+    //Student* pt = &stud1; // 定义指向基类对象的指针变量pt,指向stud1
+    //pt->display();        // 输出Student(基类)对象stud1中的数据
+    //pt = &grad1;          // pt指向Graduate类对象grad1
+    //pt->display();        // 希望输出Graduate类对象grad1中的数据
+    stud1.display();
+    grad1.display();
+    return 0;
+}
