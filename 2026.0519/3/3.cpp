5ed78f34ccff984ddba4a1a9fe5ce0905fec557e
Administrator <Administrator@JF509-05>
2026-05-19 16:30:09 +0800

    3

---
+++ 2026.0519/3/3.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    char str[20];
+    cin >> str;
+    for (int i = strlen(str);i >= 0;i--) {
+        cout.put(str[i]);
+    }
+    cout << endl;
+    return 0;
+}
