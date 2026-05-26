9c6764a11056a8f5571829c4d0fc9b53b8af9fd3
Administrator <Administrator@JF509-05>
2026-05-26 16:15:20 +0800

    3

---
+++ 2026.0526/3/3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	int num[10];
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "请输入第" << i + 1 << "个整数：";
+		cin >> num[i];
+	}
+	ofstream outfile("numbers.txt");
+	if (!outfile)
+	{
+		cerr << "Error opening file!" << endl;
+		return -1;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		outfile << num[i] << endl;
+	}
+	outfile.close();
+}
+
